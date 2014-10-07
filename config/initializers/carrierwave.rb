80bab6cd4603e5c5c57ff2f494a03049a39ba035
Adam Biagianti <adam.biagianti@gmail.com>
2014-10-06 21:40:02 -0700

    sets env vars for s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'xxx',
-    :aws_secret_access_key  => 'yyy',
+    :aws_access_key_id      => ENV['AWS_KEY'],
+    :aws_secret_access_key  => ENV['AWS_SECRET'],
     :region                 => 'us-west-2'
     #:host                   => 's3.example.com',             # optional, defaults to nil
     #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
